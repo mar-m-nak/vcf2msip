7e071ae5daa5e389bbd1678df3012ed9bfe224b6
nakami <marm.nakamura@gmail.com>
2021-02-08 07:03:52 +0900

    新規 ・vcfのパースと値の取得まで

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+mod vcf_parser;
+
+use std::fs::OpenOptions;
+use std::io::prelude::*;
+use vcf_parser::{Contact, Vcf}; //, File};
+
+fn main() {
+    // open and read vcf file
+    let _filename = "./testfiles/contacts.vcf";
+    let mut vcf = Vcf::new();
+    if let Err(_) = vcf.load(&_filename) {
+        panic!("ファイルが開けません");
+    }
+
+    // loop vcards
+    let mut count = 0;
+    for vcard in vcf.get_vcards() {
+
+        // parse contact from one vcard
+        let ct = Contact::new(&vcard);
+        if ct.is_empty() { continue; }
+        println!("{} - {:?}", ct.name_index(), ct.full_name());
+        count += 1;
+
+        // loop telephone
+        for telephone in ct.tel_iter() {
+            println!("{}:{}", telephone.get_type(), telephone.get_number());
+        }
+    }
+    println!("all: {}", count);
+
+    /*
+    let _filename = "./testfiles/output_test.xml";
+    let mut hfile = match OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(false)
+        .truncate(true)
+        .open(&_filename)
+    {
+        Ok(h) => h,
+        _ => panic!("ファイルが作成できません"),
+    };
+
+    if let Err(e) = writeln!(hfile, "A new line!100") {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+    if let Err(e) = writeln!(hfile, "A new line!101") {
+        eprintln!("Couldn't write to file: {}", e);
+    }
+    */
+}
---
+++ src/vcf_parser.rs
@@ -0,0 +1,149 @@
+pub use std::fs::File;
+use std::io::{BufReader, Read};
+use kanaria::{string::UCSStr, utils::ConvertTarget};
+use regex::Regex;
+
+#[derive(Debug)]
+pub struct Telephone {
+    teltype: String,
+    number: String,
+}
+
+#[derive(Debug)]
+pub struct Contact {
+    full_name: String,
+    name: String,
+    xfirst_name: String,
+    xlast_name: String,
+    categories: String,
+    organization: String,
+    tel_numbers: Vec<Telephone>,
+}
+
+#[derive(Debug)]
+pub struct Vcf {
+    data: String,
+}
+
+impl Vcf {
+
+    /// initialize buffer
+    pub fn new() -> Self{
+        Self{data: String::with_capacity(1048576)}
+    }
+
+    /// load vcf file to buffer
+    pub fn load(&mut self, filename: &str) -> Result<(), i32> {
+        let hfile = match File::open(&filename) {
+            Ok(h) => h,
+            _ => return Err(-1),
+        };
+        let mut reader = BufReader::new(&hfile);
+        if let Err(_) = reader.read_to_string(&mut self.data) {
+            return Err(-1);
+        }
+        Ok(())
+    }
+
+    /// return split vcards
+    pub fn get_vcards(&self) -> Vec<&str> {
+        let mut block: Vec<&str> = self.data.split("END:VCARD").collect();
+        block.pop();
+        block
+    }
+}
+
+impl Contact {
+
+    /// parse one vcard
+    pub fn new(vcard: &str) -> Self {
+        Self {
+            full_name: Self::capture("FN:", &vcard),
+            name: Self::capture("N:", &vcard),
+            xfirst_name: Self::capture("X-PHONETIC-FIRST-NAME:", &vcard),
+            xlast_name: Self::capture("X-PHONETIC-LAST-NAME:", &vcard),
+            categories: Self::capture("CATEGORIES:", &vcard),
+            organization: Self::capture(r"item[\d]*.ORG:", &vcard),
+            tel_numbers: Self::cap_tel_numbers(&vcard),
+        }
+    }
+
+    /// capture value from header line
+    fn capture(header: &str, vcard: &str) -> String {
+        let pat = format!("(?m)^{}(.*)$", &header);
+        let re = Regex::new(&pat).unwrap();
+        let raw_value = match re.captures(&vcard) {
+            None => "".to_string(),
+            Some(s) => s[1].trim_end().to_string(),
+        };
+        // convert japanese HANKAKU KATAKANA to ZENKAKU
+        UCSStr::from_str(&raw_value).wide(ConvertTarget::KATAKANA).to_string()
+    }
+
+    /// capture telephones type and number
+    fn cap_tel_numbers(vcard: &str) -> Vec<Telephone> {
+        let re = Regex::new(r"(?m)^[item\d\.]*TEL(;TYPE=([a-zA-Z]*))*:(.*)$").unwrap();
+        let mut vec_telnums:Vec<Telephone> = Vec::new();
+        for cap in re.captures_iter(&vcard) {
+            let teltype = cap.get(2).map_or("", |m| m.as_str());
+            let number = cap.get(3).map_or("", |m| m.as_str());
+            if number != "" { vec_telnums.push(
+                Telephone{
+                    teltype: teltype.to_string(),
+                    number: number.trim_end().to_string(),
+                }
+            ); }
+        }
+        vec_telnums
+    }
+
+    /// is no telephone number
+    pub fn is_empty(&self) -> bool {
+        self.tel_numbers.is_empty()
+    }
+
+    /// return full or organization name
+    pub fn full_name(&self) -> &str {
+        if !self.full_name.is_empty() {
+            self.full_name.as_ref()
+        } else {
+            self.organization.as_ref()
+        }
+    }
+
+    /// return initial from last or full or org name
+    pub fn name_index(&self) -> String {
+        let target_name = if !self.xlast_name.is_empty() {
+            &self.xlast_name
+        } else if !self.full_name.is_empty() {
+            &self.full_name
+        } else {
+            &self.organization
+        };
+        let hira = UCSStr::from_str(&target_name).hiragana().to_string();
+        let v:Vec<&str> = hira.split("").collect();
+        match v.get(1) {
+            Some(s) => s.to_string(),
+            _ => "".to_string()
+        }
+    }
+
+    pub fn categories(&self) -> &str {
+        self.categories.as_ref()
+    }
+
+    /// return telephones iterator
+    pub fn tel_iter(&self) -> impl Iterator<Item = &Telephone> {
+        self.tel_numbers.iter()
+    }
+}
+
+impl Telephone {
+    pub fn get_type(&self) -> &str {
+        self.teltype.as_ref()
+    }
+    pub fn get_number(&self) -> &str {
+        self.number.as_ref()
+    }
+}
+
